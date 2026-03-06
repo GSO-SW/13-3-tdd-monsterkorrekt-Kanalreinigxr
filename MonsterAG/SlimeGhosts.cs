da9421a5c8e4c53cdf44d038d8c19741f5a3853c
Hoger Hanara <hoger.h3@SCHULE.KOELN>
2026-03-06 11:00:32 +0100

    aktuellster Stand

---
+++ MonsterAG/SlimeGhosts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterAG
+{
+    public class SlimeGhost : Ghost
+    {
+        public SlimeGhost(string name) : base(name)
+        {
+
+        }
+
+        public string Slime()
+        {
+
+        }
+    }
+}
